79d5b1856f482c52e0ad7d469b0fe651808db5a6
Yuvraj <ysahu.atx@gmail.com>
2020-02-11 19:34:02 -0600

    Edited GraphHelper class

--- GraphHelper.java
+++ GraphHelper.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-class Grapher extends Canvas {
+public class GraphHelper extends Canvas {
 
     JFrame frame; //This is not private so that classes that extend this class can have more choice
     Graphics2D graphics;
@@ -9,7 +9,7 @@ class Grapher extends Canvas {
     final static int DEFAULT_WINDOW_DIMENSION = 500;
     final static String DEFAULT_WINDOW_NAME = "My Drawing";
 
-    Grapher(String frameTitle, int windowWidth, int windowHeight) {
+    GraphHelper(String frameTitle, int windowWidth, int windowHeight) {
         frame = new JFrame(frameTitle);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setSize(windowWidth, windowHeight);
@@ -20,15 +20,15 @@ class Grapher extends Canvas {
     }
 
     //These constructors are not used but are helpful constructors for users
-    Grapher() {
+    GraphHelper() {
         this(DEFAULT_WINDOW_NAME, DEFAULT_WINDOW_DIMENSION, DEFAULT_WINDOW_DIMENSION);
     }
 
-    Grapher(int windowWidth, int windowHeight) {
+    GraphHelper(int windowWidth, int windowHeight) {
         this(DEFAULT_WINDOW_NAME, windowWidth, windowHeight);
     }
 
-    Grapher(String name) {
+    GraphHelper(String name) {
         this(name, DEFAULT_WINDOW_DIMENSION, DEFAULT_WINDOW_DIMENSION);
     }
 
