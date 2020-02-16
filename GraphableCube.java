258243062aa138282f02901c50204ac1463f439a
Yuvraj <ysahu.atx@gmail.com>
2020-02-16 16:51:31 -0600

    Can also adjust rotation and translation intervals

--- GraphableCube.java
+++ GraphableCube.java
@@ -6,9 +6,18 @@ public class GraphableCube extends GraphHelper implements KeyListener {
 
     private Cube cube;
 
+    //Constants
+
+    //These are for the max/min rotation/translation intervals
+    public static final int MAX_ROTATION = 90, MAX_TRANSLATION = 50,
+                            MIN_ROTATION = 5, MIN_TRANSLATION = 5;
+
+    //These are the increment sizes for translation/rotation
+    public static final int ROTATION_INC = 5, TRANSLATION_INC = 5;
+
     private int xDistToCenter, yDistToCenter;
-    private int rotationInterval = 15; //Will be controlled by keys in future updates
-    private int translationInterval = 50; //Will also be controlled by keys in future updates
+    private int rotationInterval = 15;
+    private int translationInterval = 20;
 
     private boolean shiftPressed = false;
 
@@ -63,6 +72,31 @@ public class GraphableCube extends GraphHelper implements KeyListener {
         }
     }
 
+    /*
+    CURRENT KEY MAPPINGS:
+
+    W - positive X rotation
+    S - negative X rotation
+    D - positive Y rotation
+    A - negative Y rotation
+
+    NOTE: down correlates to increasing Y values
+
+    Shift+W - negative Y translation
+    Shift+S - positive Y translation
+    Shift+D - positive X translation
+    Shift+A - negative X translation
+
+    Right_Arrow - positive Z rotation
+    Left_Arrow - negative Z rotation
+
+    Up_Arrow - increased rotation interval
+    Down_Arrow - decreased rotation interval
+
+    Shift+Up_Arrow - increased translation interval
+    Shift+Down_Arrow - decreased translation interval
+     */
+
     public void keyPressed(KeyEvent event) {
         switch(event.getKeyCode()) {
             case KeyEvent.VK_SHIFT : {
@@ -95,6 +129,30 @@ public class GraphableCube extends GraphHelper implements KeyListener {
             }
             case KeyEvent.VK_LEFT : {
                 rotateAndPaint(Cube.RotationalAxis.Z, -rotationInterval);
+                break;
+            }
+            case KeyEvent.VK_UP : {
+                if (shiftPressed) {
+                    if (translationInterval < MAX_TRANSLATION) {
+                        translationInterval += TRANSLATION_INC;
+                    }
+                } else {
+                    if (rotationInterval < MAX_ROTATION) {
+                        rotationInterval += ROTATION_INC;
+                    }
+                }
+                break;
+            }
+            case KeyEvent.VK_DOWN : {
+                if (shiftPressed) {
+                    if (translationInterval > MIN_TRANSLATION) {
+                        translationInterval -= TRANSLATION_INC;
+                    }
+                } else {
+                    if (rotationInterval > MIN_ROTATION) {
+                        rotationInterval -= ROTATION_INC;
+                    }
+                }
                 break;
             }
         }
